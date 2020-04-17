09d463dff19f0462247b096611a16d42abdd7049
Apurva Nisal <apunisal@gmail.com>
2020-04-17 12:24:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<!--[if IE 9]><html class="ie9"><![endif]-->
-<!--[if gt IE 9]><!-->
 <html>
-<!--<![endif]-->
-  <head>
-   <title> Apu <title>
-  
-  </head>
-  <body>
-   
-          <h1>Welcome to an OpenShift Application! I am apu!!! </h1>
-        
-  </body>
+
+<head>
+  <title>Apu....</title>
+</head>
+
+<body>
+<h1>openshift Apu</h1> 
+</body>
+
 </html>
